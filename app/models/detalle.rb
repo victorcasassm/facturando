7dc2b3af21ada5cda577c3b86c3faed2ee134a05
Víctor Casas <victor.casas.sm@gmail.com>
2014-09-15 07:44:03 +0200

    validates_numericality_of

--- app/models/detalle.rb
+++ app/models/detalle.rb
@@ -1,4 +1,5 @@
 class Detalle < ActiveRecord::Base
   belongs_to :factura
   validates_presence_of :producto, :cantidad
+  validates_numericality_of :cantidad
 end
--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,4 +1,5 @@
 class Producto < ActiveRecord::Base
     belongs_to :cliente
     validates_presence_of :nombre, :precio, :cantidad
+    validates_numericality_of :precio, :cantidad
 end
